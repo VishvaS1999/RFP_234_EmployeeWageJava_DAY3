49105eea9b0188122b917d35e09157776fe5dab5
Vishva <gvishva2601@gmail.com>
2022-12-04 17:12:25 +0530

    Welcome Employee Wage Computation Program

---
+++ src/com/EmployeeWageDay3/WelcomeEmployeeWage.java
@@ -0,0 +1,9 @@
+package com.EmployeeWageDay3;
+
+public class WelcomeEmployeeWage
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome Employee Wage Computation Program!..");
+    }
+}
