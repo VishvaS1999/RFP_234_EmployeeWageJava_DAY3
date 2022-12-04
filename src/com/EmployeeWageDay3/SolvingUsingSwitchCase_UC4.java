50fb448a7197fa4fe42bb3930a15f72bcb14258b
Vishva <gvishva2601@gmail.com>
2022-12-04 17:20:28 +0530

    Solving Using Switch Case

---
+++ src/com/EmployeeWageDay3/SolvingUsingSwitchCase_UC4.java
@@ -0,0 +1,34 @@
+package com.EmployeeWageDay3;
+
+public class SolvingUsingSwitchCase_UC4
+{
+    public static void main(String[] args)
+    {
+        //constant
+         final int IS_PART_TIME = 1;
+         final int IS_FULL_TIME = 2;
+         final int EMP_RATE_PER_HOUR = 20;
+
+        //variables
+        int empHrs = 0;
+        int empWage = 0;
+
+        int empCheck=(int) Math.floor(Math.random()*10) %3;
+
+        switch (empCheck)
+        {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+
+            default:
+                empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage Using Switch Case is :- " +empWage);
+    }
+}
